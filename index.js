c699fe1600429482633c464bd4aef7afc81fdec5
binhussen <mhdhsna@gmail.com>
2022-07-18 17:48:40 +0300

    Add nav-link menu toggle

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+/** @format */
+
 const harmburger = document.querySelector(".harmburger");
 const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
 harmburger.addEventListener("click", (e) => {
-  navMenu.classList.toggle("slide");
-  harmburger.classList.toggle("slide");
+	navMenu.classList.toggle("slide");
+	harmburger.classList.toggle("slide");
 });
+
+navLink.forEach((n) =>
+	n.addEventListener("click", () => {
+		harmburger.classList.remove("slide");
+		navMenu.classList.remove("slide");
+	})
+);
